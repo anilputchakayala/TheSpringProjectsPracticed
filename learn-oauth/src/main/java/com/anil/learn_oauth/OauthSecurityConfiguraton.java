fe59c82bc5607f1a32e939fe0feae8ef9146ca02
Anil Putchakayala <anil252anil@gmail.com>
2026-05-20 17:07:40 +0530

    OAuth2

---
+++ learn-oauth/src/main/java/com/anil/learn_oauth/OauthSecurityConfiguraton.java
@@ -0,0 +1,22 @@
+package com.anil.learn_oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OauthSecurityConfiguraton {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+        http.oauth2Login(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+}
